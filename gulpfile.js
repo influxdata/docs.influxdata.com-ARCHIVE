0c92b0e001a29f65bd720815fe1cacf8ad30235d
Alex P <thealexpaxton@gmail.com>
2016-03-16 11:52:05 -0700

    Adding some cool plugins to gulp file
    Not sure if these are in the right order, will have to test later

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,19 @@
 var gulp = require('gulp'),
     less = require('gulp-less'),
+    autoprefix = require('gulp-autoprefixer'),
     livereload = require('gulp-livereload'),
-    watch = require('gulp-watch');
+    watch = require('gulp-watch'),
+    cleanCSS = require('gulp-clean-css'),
+    sourcemaps = require('gulp-sourcemaps');
 
 gulp.task('less', function() {
    gulp.src('*.less')
       .pipe(watch())
       .pipe(less())
+      .pipe(sourcemaps.init())
+      .pipe(cleanCSS())
+      .pipe(sourcemaps.write())
+      .pipe(autoprefix('last 2 version', 'ie 8', 'ie 9'))
       .pipe(gulp.dest('static/css/docs-default.css'))
       .pipe(livereload());
 });
