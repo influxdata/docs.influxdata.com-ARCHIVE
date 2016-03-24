3cbebc7ae03997635973e4fcd1c66672f60b6d84
Alex P <thealexpaxton@gmail.com>
2016-03-24 16:05:28 -0700

    Functional theme switching
    Going to add special icons for these next

--- static/js/docs-interactions.js
+++ static/js/docs-interactions.js
@@ -57,3 +57,70 @@ $(function(){
 	var imagePathString = "/img/header-images/header-image-"+getRandomIntInclusive(1,14)+".jpg";
 	$('#page-title').css('background-image','url('+imagePathString+')');
 });
+
+
+/*
+	Copied and pasted this script for CSS swaps w/ cookies from
+	http://www.thesitewizard.com/javascripts/change-style-sheets.shtml
+*/
+
+// *** TO BE CUSTOMISED ***
+
+var style_cookie_name = "influx-docs-theme" ;
+var style_cookie_duration = 30 ;
+var style_domain = "docs.influxdata.com" ;
+
+// *** END OF CUSTOMISABLE SECTION ***
+// You do not need to customise anything below this line
+
+function switch_style ( css_title )
+{
+// You may use this script on your site free of charge provided
+// you do not remove this notice or the URL below. Script from
+// http://www.thesitewizard.com/javascripts/change-style-sheets.shtml
+  var i, link_tag ;
+  for (i = 0, link_tag = document.getElementsByTagName("link") ;
+    i < link_tag.length ; i++ ) {
+    if ((link_tag[i].rel.indexOf( "stylesheet" ) != -1) &&
+      link_tag[i].title) {
+      link_tag[i].disabled = true ;
+      if (link_tag[i].title == css_title) {
+        link_tag[i].disabled = false ;
+      }
+    }
+    set_cookie( style_cookie_name, css_title,
+      style_cookie_duration, style_domain );
+  }
+}
+function set_style_from_cookie()
+{
+  var css_title = get_cookie( style_cookie_name );
+  if (css_title.length) {
+    switch_style( css_title );
+  }
+}
+function set_cookie ( cookie_name, cookie_value,
+    lifespan_in_days, valid_domain )
+{
+    // http://www.thesitewizard.com/javascripts/cookies.shtml
+    var domain_string = valid_domain ?
+                       ("; domain=" + valid_domain) : '' ;
+    document.cookie = cookie_name +
+                       "=" + encodeURIComponent( cookie_value ) +
+                       "; max-age=" + 60 * 60 *
+                       24 * lifespan_in_days +
+                       "; path=/" + domain_string ;
+}
+function get_cookie ( cookie_name )
+{
+    // http://www.thesitewizard.com/javascripts/cookies.shtml
+    var cookie_string = document.cookie ;
+    if (cookie_string.length != 0) {
+        var cookie_value = cookie_string.match (
+                        '(^|;)[\s]*' +
+                        cookie_name +
+                        '=([^;]*)' );
+        return decodeURIComponent ( cookie_value[2] ) ;
+    }
+    return '' ;
+}
