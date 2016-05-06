7e9618a63f2174e7ceeb44116b2d91f006202498
Todd Persen <todd.persen@gmail.com>
2016-05-06 15:29:20 -0700

    Switch to js-cookies for setting/getting cookies.

--- static/js/docs-interactions.js
+++ static/js/docs-interactions.js
@@ -81,46 +81,21 @@ function switch_style ( css_title )
   var i, link_tag ;
   for (i = 0, link_tag = document.getElementsByTagName("link") ;
     i < link_tag.length ; i++ ) {
-    if ((link_tag[i].rel.indexOf( "stylesheet" ) != -1) &&
+    if ((link_tag[i].rel.indexOf("stylesheet") != -1) &&
       link_tag[i].title) {
       link_tag[i].disabled = true ;
       if (link_tag[i].title == css_title) {
         link_tag[i].disabled = false ;
       }
     }
-    set_cookie( style_cookie_name, css_title,
-      style_cookie_duration, style_domain );
+    Cookies.set(style_cookie_name, css_title);
   }
 }
+
 function set_style_from_cookie()
 {
-  var css_title = get_cookie( style_cookie_name );
-  if (css_title.length) {
-    switch_style( css_title );
+  var css_title = Cookies.get(style_cookie_name);
+  if (css_title !== undefined) {
+    switch_style(css_title);
   }
-}
-function set_cookie ( cookie_name, cookie_value,
-    lifespan_in_days, valid_domain )
-{
-    // http://www.thesitewizard.com/javascripts/cookies.shtml
-    var domain_string = valid_domain ?
-                       ("; domain=" + valid_domain) : '' ;
-    document.cookie = cookie_name +
-                       "=" + encodeURIComponent( cookie_value ) +
-                       "; max-age=" + 60 * 60 *
-                       24 * lifespan_in_days +
-                       "; path=/" + domain_string ;
-}
-function get_cookie ( cookie_name )
-{
-    // http://www.thesitewizard.com/javascripts/cookies.shtml
-    var cookie_string = document.cookie ;
-    if (cookie_string.length != 0) {
-        var cookie_value = cookie_string.match (
-                        '(^|;)[\s]*' +
-                        cookie_name +
-                        '=([^;]*)' );
-        return decodeURIComponent ( cookie_value[2] ) ;
-    }
-    return '' ;
 }
